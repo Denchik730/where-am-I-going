9d4c57ad7173dc8ff0234fd662410c99ed705e5c
Denis Gabbasov <gabbas.denn@mail.ru>
2022-11-05 14:31:03 +0300

    finished project

--- js/script.js
+++ js/script.js
@@ -2,17 +2,23 @@ window.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('.header'),
   headerLink = document.querySelectorAll('.header__link'),
   headerLinkHate = document.querySelector('.header__link-hate'),
-  hamburger = document.querySelector('.hamburger');
+  hamburger = document.querySelector('.hamburger'),
+  headerDividers = hamburger.querySelectorAll('.hamburger__divider');
 
   hamburger.addEventListener('click', () => {
       // hamburger.classList.toggle('hamburger_active');
       header.classList.toggle('header_active');
+      headerDividers.forEach(divider => {
+        divider.classList.toggle('hamburger__divider_active')
+      })
   });
 
   headerLink.forEach(item => {
       item.addEventListener('click', () => {
-          hamburger.classList.toggle('hamburger_active');
           header.classList.toggle('header_active');
+          headerDividers.forEach(divider => {
+            divider.classList.toggle('hamburger__divider_active')
+          })
       })
 
   })
